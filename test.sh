83486daf8e2d87922c963e3e04c722b5f473284e
Joël Galeran <Jolg42@users.noreply.github.com>
2023-12-04 20:03:16 +0100

    push to netlify: 1701716594N, engine = library

--- test.sh
+++ test.sh
@@ -10,4 +10,4 @@ else
   files=',"files":["deno","edge.d.ts","edge.js","index-browser.js","index.d.ts","index.js","libquery_engine-rhel-openssl-1.0.x.so.node","package.json","schema.prisma"]'
 fi
 
-pnpm ts-node ../../utils/fetch-retry-and-confirm-version.ts --url https://$ID--prisma-ecosystem-tests-netlify-github.netlify.app/.netlify/functions/index --prisma-version $(sh ../../utils/prisma_version.sh) --binary-string $files
+pnpm ts-node ../../utils/fetch-retry-and-confirm-version.ts --url https://$ID--gentle-jalebi-42db3f.netlify.app/.netlify/functions/index --prisma-version $(sh ../../utils/prisma_version.sh) --binary-string $files
