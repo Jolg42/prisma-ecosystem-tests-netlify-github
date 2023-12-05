358e4f55da7ebc740d5d1cec4f47a75a6bc5467c
Joël Galeran <Jolg42@users.noreply.github.com>
2023-12-05 10:51:09 +0100

    push to netlify: 1701769867N, engine = library

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const version = process.version
-const { NETLIFY_BETA_PG_URL, ...env } = process.env
+const { NETLIFY_BETA_PG_URL, AWS_SECRET_ACCESS_KEY, AWS_SESSION_TOKEN, AWS_ACCESS_KEY_ID, ...env } = process.env
 
 console.debug(
   JSON.stringify(
