3a43ac70639a1610d6899e3fe563aa63cd2d3c36
Joël Galeran <Jolg42@users.noreply.github.com>
2023-12-05 16:08:36 +0100

    push to netlify: 1701788913N, engine = library

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const fs = require('fs')
+const path = require('path')
+
 const version = process.version
 const { NETLIFY_BETA_PG_URL, AWS_SECRET_ACCESS_KEY, AWS_SESSION_TOKEN, AWS_ACCESS_KEY_ID, ...env } = process.env
 
@@ -20,8 +23,6 @@ fs.copyFileSync(
 console.log({ filestmp })
 
 // list all files in node_modules/.prisma/client
-const fs = require('fs')
-const path = require('path')
 const dirPath = path.dirname(require.resolve('.prisma/client'))
 const files = fs.readdirSync(dirPath)
 console.log({ dirPath })
