8de8fd98aa3870539b62587e38e16a068fd2b171
Joël Galeran <Jolg42@users.noreply.github.com>
2023-12-05 16:31:50 +0100

    push to netlify: 1701790308N, engine = library

--- script.js
+++ script.js
@@ -16,11 +16,18 @@ console.debug(
 )
 
 const filestmp = fs.readdirSync('/tmp/prisma-engines')
-fs.copyFileSync(
-  '/tmp/prisma-engines/libquery_engine-rhel-openssl-3.0.x.so.node',
-  '/opt/build/repo/node_modules/.prisma/client/libquery_engine-rhel-openssl-3.0.x.so.node',
-)
 console.log({ filestmp })
+const sourceDir = '/tmp/prisma-engines'
+const destDir = '/opt/build/repo/node_modules/.prisma/client'
+const library1xName = 'libquery_engine-rhel-openssl-1.0.x.so.node'
+const library3xName = 'libquery_engine-rhel-openssl-3.0.x.so.node'
+
+// Copy the 3.0.x engine to the expected location
+fs.copyFileSync(path.join(sourceDir, library3xName), path.join(destDir, library3xName))
+
+// Delete the 1.0.x engine
+// Only log if that fails
+await fs.unlink(path.join(destDir, library1xName)).catch((e) => console.log(e))
 
 // list all files in node_modules/.prisma/client
 const dirPath = path.dirname(require.resolve('.prisma/client'))
